b3de8a994f8c14804341abc3d5843ea519f5eae7
Hugo Mellouet <hugo.mellouet@gmail.com>
2022-03-28 14:55:45 +0200

    rebase

--- front/src/App.js
+++ front/src/App.js
@@ -1,21 +1,20 @@
 import React from "react";
-import "./App.css";
-import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import About from "./pages/about";
 import Import from "./pages/import";
 import TryOn from "./pages/tryon";
 import Wardrobe from "./pages/wardrobe";
+import Navbar from "./components/navbar/navbar";
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
+        <Route path="/" element={<Wardrobe />} />
         <Route path="/about" element={<About />} />
         <Route path="/import" element={<Import />} />
         <Route path="/tryon" element={<TryOn />} />
-        <Route path="/wardrobe" element={<Wardrobe />} />
       </Routes>
     </Router>
   );
--- front/src/pages/wardrobe.js
+++ front/src/pages/wardrobe.js
@@ -1,34 +1,25 @@
-import React from 'react';
-import "./wardrobe.css"
+import React from "react";
+import "./wardrobe.css";
 const Wardrobe = () => {
-return (
-	<div class="home-section">
-	    <h1 class="logotitle"><a href="#">Wardrobe</a></h1>
-	    <header><nav>
-                            <li><a href="#">Import</a></li>
-                            <li><a href="#">About us</a></li>
-                            <li><a href="#"><img src={require("./img/logo.png")}></img></a></li>
-                        </nav>
-	    </header>
-        <body>
-            <div class="container">
-                <div class="banner">
-                    <div class="titlebanner">The modern way to style yourself.</div>
-                    <div class="descbanner">We are bringing a digital experience to the fashion world.</div>
-                    <button class="buttonbanner">Try Now</button>
-                </div>
-                <img src={require("./img/femme.png")}></img>
-                <div class="arrows">
-                    <div class="leftarrows">
-                    </div>
-                    <div class="rightarrows">
-                    </div>
-                </div>
+  return (
+    <div class="home-section">
+      <body>
+      <h1 className="logotitle">W@rdrobe</h1>
+      <div class="container">
+          <div class="banner">
+            <div class="titlebanner">The modern way<br/>to style yourself.</div>
+            <div class="descbanner">
+              We are bringing a digital experience to the<br/>fashion world.
             </div>
-        </body>
+            <a href="/tryon">
+              <button className="buttonbanner" hre>Try Now</button>
+            </a>
+          </div>
+          <img className="mannequin" src={require("./img/femme.png")}></img>
+        </div>
+      </body>
     </div>
   );
 };
 
 export default Wardrobe;
-
